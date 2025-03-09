02902cd7c106f3cc6bbef4d653bb1bb22a04ba50
tresor <110840070+thumanicsgroug@users.noreply.github.com>
2025-03-09 18:19:05 +0000

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,24 @@ import { LayoutComponent } from './layouts/layout/layout.component';
 import { VexRoutes } from '@vex/interfaces/vex-route.interface';
 
 export const appRoutes: VexRoutes = [
-  {
+    {
     path: '',
+    loadChildren: () => import('./auth/auth-routes'),
+  },
+  {
+    path: 'index',
     component: LayoutComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+    ]
   }
 ];

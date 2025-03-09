397b24f5ce73fc22cf0dae710615c1cbdf75db1b
tresor <110840070+thumanicsgroug@users.noreply.github.com>
2025-03-09 19:04:25 +0000

    update dashboard subtoolbar

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { VexBreadcrumbsComponent } from '@vex/components/vex-breadcrumbs/vex-breadcrumbs.component';
+import { VexSecondaryToolbarComponent } from '@vex/components/vex-secondary-toolbar/vex-secondary-toolbar.component';
 
 @Component({
   selector: 'vex-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  standalone: true,
+  imports: [
+    VexSecondaryToolbarComponent,
+    VexBreadcrumbsComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class DashboardComponent {
 

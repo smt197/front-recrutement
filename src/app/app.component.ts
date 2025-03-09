3aa3b4cd726c90999ee51ba7e1a7c4be248db3f5
tresor <110840070+thumanicsgroug@users.noreply.github.com>
2025-03-09 18:20:44 +0000

    configuration du theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VexColorScheme, VexConfigName } from '@vex/config/vex-config.interface';
+import { VexConfigService } from '@vex/config/vex-config.service';
 
 @Component({
   selector: 'vex-root',
@@ -7,4 +9,66 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(
+    private configService: VexConfigService
+  ) {
+    this.configuretheme();
+  }
+  ngOnInit(): void {
+
+  };
+
+
+  configuretheme() {
+    this.configService.updateConfig({
+      id: VexConfigName.ares,
+      name: 'Ares',
+      bodyClass: 'vex-layout-ares',
+      imgSrc: '//vex-landing.visurel.com/assets/img/layouts/ares.png',
+      direction: "ltr",
+      showSettingTheme: {
+        visible: false
+      },      
+      style: {
+        colorScheme: VexColorScheme.LIGHT,
+        button: {
+          borderRadius: undefined,
+        },
+      },
+      layout: "horizontal",
+      boxed: false,
+      sidenav: {
+        title: "resurex",
+        imageUrl: "assets/img/logo/logo.svg",
+        showCollapsePin: true,
+        user: {
+          visible: false
+        },
+        search: {
+          visible: false
+        },
+        state: 'expanded'
+      },
+      toolbar: {
+        fixed: true,
+        user: {
+          visible: true,
+        },
+        search: {
+          visible: false
+        },
+        panel: {
+          visible: false
+        },
+      },
+      footer: {
+        visible: false,
+        fixed: true
+      }
+    });
+  }
+}
+
+

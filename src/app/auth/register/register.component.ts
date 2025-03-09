c91f3f0896bb8e0874ee52020e285b9b7b77d44e
tresor <110840070+thumanicsgroug@users.noreply.github.com>
2025-03-09 16:16:44 +0000

    create register component

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,89 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators
+} from '@angular/forms';
+import { fadeInUp400ms } from '@vex/animations/fade-in-up.animation';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'vex-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../auth.component.scss'],
+  animations: [fadeInUp400ms],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    NgIf,
+    MatIconModule,
+    MatCheckboxModule,
+    MatStepperModule,
+  ]
+})
+export class RegisterComponent {
+  accountFormGroup: UntypedFormGroup = this.fb.group({
+    first_name: [null, [Validators.required, Validators.minLength(4),Validators.maxLength(50),Validators.pattern('^[a-zA-ZÀ-ÿ \'-]+$')]],
+    last_name: [null, [Validators.required, Validators.minLength(4),Validators.maxLength(50),Validators.pattern('^[a-zA-ZÀ-ÿ \'-]+$')]],
+    email: ['', [
+      Validators.required, 
+      Validators.minLength(8), 
+      Validators.maxLength(254),
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+    ]],
+  });
+
+  passwordFormGroup: UntypedFormGroup = this.fb.group({
+    password: [
+      null,
+      Validators.compose([Validators.required, Validators.minLength(6)])
+    ],
+    passwordConfirm: [null, Validators.required]
+  });
+
+  confirmFormGroup: UntypedFormGroup = this.fb.group({
+    terms: [null, Validators.requiredTrue]
+  });
+
+  passwordInputType = 'password';
+
+  constructor(
+    private fb: UntypedFormBuilder,
+    private cd: ChangeDetectorRef,
+    private snackbar: MatSnackBar
+  ) {}
+
+  showPassword() {
+    this.passwordInputType = 'text';
+    this.cd.markForCheck();
+  }
+
+  hidePassword() {
+    this.passwordInputType = 'password';
+    this.cd.markForCheck();
+  }
+
+  submit() {
+    this.snackbar.open(
+      'Hooray! You successfully created your account.',
+      undefined,
+      {
+        duration: 5000
+      }
+    );
+  }
+}

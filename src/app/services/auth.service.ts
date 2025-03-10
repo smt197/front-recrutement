978f763fbdeb7eaf36632f0a81e137422c92109a
smt197 <105449441+smt197@users.noreply.github.com>
2025-03-10 23:58:48 +0000

    service http

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api/register'; // Ton URL d'API Laravel
+  private apiUrl = 'http://127.0.0.1:8000/api/register';
 
   constructor(private http: HttpClient) {}
 

b778b63732724f0455c1e3ee65854c309bd24d33
tresor <110840070+thumanicsgroug@users.noreply.github.com>
2025-03-12 13:53:23 +0000

    Update Response-globalServer.ts

--- src/app/interfaces/Response-globalServer.ts
+++ src/app/interfaces/Response-globalServer.ts
@@ -3,5 +3,7 @@ import { User } from "@ngneat/falso";
 export interface ResponseGlobalServer {
     data?: User|any;
     message?: string;
+    mailmessage?: string;
     statut?: boolean;
+    email_verified?: boolean;
 }
